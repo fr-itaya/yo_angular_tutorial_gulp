92d17115e15c8ff7dfbd3c74cab12004697b0fd4
fr-itaya <itaya@fact-real.com>
2015-06-25 10:44:15 +0000

    added todo list and register form. #1

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -2,38 +2,14 @@
   'use strict';
 
   angular
-    .module('angularTodoGulp')
-    .controller('MainController', MainController);
+  .module('angularTodoGulp')
+  .controller('MainController', function($scope) {
+    $scope.todos = ['Item1', 'Item2', 'Item3'];
 
-  /** @ngInject */
-  function MainController($timeout, webDevTec, toastr) {
-    var vm = this;
+    $scope.addTodo = function() {
+      $scope.todos.push($scope.todo);
+      $scope.todo = '';
+    };
+  });
 
-    vm.awesomeThings = [];
-    vm.classAnimation = '';
-    vm.creationDate = 1435223501675;
-    vm.showToastr = showToastr;
-
-    activate();
-
-    function activate() {
-      getWebDevTec();
-      $timeout(function() {
-        vm.classAnimation = 'rubberBand';
-      }, 4000);
-    }
-
-    function showToastr() {
-      toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
-      vm.classAnimation = '';
-    }
-
-    function getWebDevTec() {
-      vm.awesomeThings = webDevTec.getTec();
-
-      angular.forEach(vm.awesomeThings, function(awesomeThing) {
-        awesomeThing.rank = Math.random();
-      });
-    }
-  }
 })();
